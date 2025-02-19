9de8ccf72f57d427c6ec89602ae20906ac9b7cc5
DQuinn97 <quinten2407@gmail.com>
2025-02-19 16:54:04 +0100

    tictactoe klaar

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import inquirer from "inquirer";
+import rl from "readline-sync";
+import chalk from "chalk";
 const board = [
   [" ", " ", " "],
   [" ", " ", " "],
@@ -7,50 +9,80 @@ const board = [
 
 const players = ["O", "X"];
 let currentActivePlayer = true;
+
+let gameRunning = true;
+const regex = new RegExp(/ *([012]) *, *([012]) */);
+let temp = " ";
+
+const pointer = [1, 1];
 const activePlayer = () => {
   currentActivePlayer = !currentActivePlayer;
   return players[+!currentActivePlayer];
 };
 const renderBoard = () => {
+  console.clear();
+  const chalkBoard = board.map((rowVal, row) =>
+    rowVal.map((colVal, col) =>
+      row == pointer[0] && col == pointer[1] ? chalk.red(temp) : colVal
+    )
+  );
+
   const row = (nr, arr) => `${nr} | ${arr.join(" | ")} |`;
   const line = `  +-----------+`;
   console.log(
     [
       `    0   1   2`,
       line,
-      row(0, board[0]),
+      row(0, chalkBoard[0]),
       line,
-      row(1, board[1]),
+      row(1, chalkBoard[1]),
       line,
-      row(2, board[2]),
+      row(2, chalkBoard[2]),
       line,
     ].join("\n")
   );
 };
 
-let gameRunning = true;
-const regex = new RegExp(/ *([012]) *, *([012]) */);
-let temp;
-
 do {
-  renderBoard();
-
   temp = activePlayer();
   let getCoords = (input) => input.coords.split(",").map((c) => +c.trim());
 
-  const input = await inquirer.prompt([
-    {
-      type: "input",
-      name: "coords",
-      message: `Player ${temp}, enter coordinates (x,y): `,
-      validate: (input) => {
-        let [match, x, y] = input.match(regex) ?? [];
-        return match && board[x][y] == " " ? true : "Please enter valid coords";
-      },
-    },
-  ]);
+  renderBoard();
+  //   const input = await inquirer.prompt([
+  //     {
+  //       type: "input",
+  //       name: "coords",
+  //       message: `Player ${temp}, enter coordinates (x,y): `,
+  //       validate: (input) => {
+  //         let [match, x, y] = input.match(regex) ?? [];
+  //         return match && board[x][y] == " " ? true : "Please enter valid coords";
+  //       },
+  //     },
+  //   ]);
 
-  let [x, y] = getCoords(input);
+  const confirm = " ";
+  let key;
+  while (key !== confirm) {
+    key = rl.keyIn("Use zqsd to select a position, press space to enter: ");
+    switch (key) {
+      case "z":
+        pointer[0] = Math.max(0, --pointer[0]);
+        break;
+      case "q":
+        pointer[1] = Math.max(0, --pointer[1]);
+        break;
+      case "s":
+        pointer[0] = Math.min(++pointer[0], 2);
+        break;
+      case "d":
+        pointer[1] = Math.min(++pointer[1], 2);
+        break;
+    }
+
+    renderBoard();
+  }
+
+  let [x, y] = pointer;
   console.log(x, y);
 
   board[x][y] = temp;
