4497ce5bafdc6a8c37ec7b6926dc0f455476bd8f
DQuinn97 <quinten2407@gmail.com>
2025-02-20 12:44:17 +0100

    total refactor, wasd functionality, win/draw conditions and added comments

--- index.js
+++ index.js
@@ -1,73 +1,73 @@
 import inquirer from "inquirer";
 import rl from "readline-sync";
 import chalk from "chalk";
+
+// Player related variables
+const playerTokens = ["O", "X"];
+let activePlayer = true;
+let activePlayerToken = " ";
+
+// Game related variables
+let gameOver = false;
 const board = [
   [" ", " ", " "],
   [" ", " ", " "],
   [" ", " ", " "],
 ];
-
-const players = ["O", "X"];
-let currentActivePlayer = true;
-
-let gameRunning = true;
-const regex = new RegExp(/ *([012]) *, *([012]) */);
-let temp = " ";
-
 const pointer = [1, 1];
-const activePlayer = () => {
-  currentActivePlayer = !currentActivePlayer;
-  return players[+!currentActivePlayer];
+const winConditions = ["OOO", "XXX"];
+let drawCondition = false;
+
+// Game functions
+const toggleActivePlayer = () => {
+  activePlayer = !activePlayer;
+  return playerTokens[+!activePlayer];
 };
 const renderBoard = () => {
-  console.clear();
+  console.clear(); //clear board before re-rendering
   const chalkBoard = board.map((rowVal, row) =>
     rowVal.map((colVal, col) =>
-      row == pointer[0] && col == pointer[1] ? chalk.red(temp) : colVal
+      row == pointer[0] && col == pointer[1]
+        ? chalk.red(activePlayerToken)
+        : colVal
     )
-  );
+  ); //map pointer onto board using chalk
 
-  const row = (nr, arr) => `${nr} | ${arr.join(" | ")} |`;
-  const line = `  +-----------+`;
+  const boardRow = (nr, arr) => `${nr} | ${arr.join(" | ")} |`;
+  const boardLine = `  +-----------+`;
+  let row = 0;
   console.log(
     [
       `    0   1   2`,
-      line,
-      row(0, chalkBoard[0]),
-      line,
-      row(1, chalkBoard[1]),
-      line,
-      row(2, chalkBoard[2]),
-      line,
+      boardLine,
+      boardRow(row, chalkBoard[row++]),
+      boardLine,
+      boardRow(row, chalkBoard[row++]),
+      boardLine,
+      boardRow(row, chalkBoard[row++]),
+      boardLine,
     ].join("\n")
   );
 };
 
 do {
-  temp = activePlayer();
-  let getCoords = (input) => input.coords.split(",").map((c) => +c.trim());
+  activePlayerToken = toggleActivePlayer(); //toggle active player and return token (X or O)
 
-  renderBoard();
-  //   const input = await inquirer.prompt([
-  //     {
-  //       type: "input",
-  //       name: "coords",
-  //       message: `Player ${temp}, enter coordinates (x,y): `,
-  //       validate: (input) => {
-  //         let [match, x, y] = input.match(regex) ?? [];
-  //         return match && board[x][y] == " " ? true : "Please enter valid coords";
-  //       },
-  //     },
-  //   ]);
+  renderBoard(); //render board before player input
 
-  const confirm = " ";
   let key;
-  while (key !== confirm) {
-    key = rl.keyIn("Use zqsd to select a position, press space to enter: ");
+  let continueCheck = true;
+
+  while (continueCheck) {
+    key = rl.keyIn(
+      "Use zqsd/wasd to select a position, press space to enter: "
+    );
     switch (key) {
+      case "w":
       case "z":
         pointer[0] = Math.max(0, --pointer[0]);
         break;
+      case "a":
       case "q":
         pointer[1] = Math.max(0, --pointer[1]);
         break;
@@ -77,34 +77,39 @@ do {
       case "d":
         pointer[1] = Math.min(++pointer[1], 2);
         break;
+      case " ": // check if place in board is filled in already, if not: continue and process input
+        if (board[pointer[0]][pointer[1]] == " ") continueCheck = false;
+        break;
     }
 
-    renderBoard();
+    renderBoard(); // re-render board after every key press
   }
 
   let [x, y] = pointer;
-  console.log(x, y);
+  board[x][y] = activePlayerToken;
 
-  board[x][y] = temp;
-
-  if (!board.flat().some((e) => e == " ")) gameRunning = false;
-
-  const checkRow = (b) =>
-    b.some((row) => ["OOO", "XXX"].includes(row.join("")));
+  const checkRows = (b) =>
+    b.some((row) => winConditions.includes(row.join("")));
   const transpose = (b) =>
     b[0].map((_, colIndex) => b.map((row) => row[colIndex])).reverse();
 
   const checkDiagonal = (b) =>
-    ["OOO", "XXX"].includes([b[0][0], b[1][1], b[2][2]].join(""));
+    winConditions.includes([b[0][0], b[1][1], b[2][2]].join(""));
 
   if (
-    checkRow(board) ||
-    checkRow(transpose(board)) ||
+    checkRows(board) ||
+    checkRows(transpose(board)) ||
     checkDiagonal(board) ||
     checkDiagonal(transpose(board))
   )
-    gameRunning = false;
-} while (gameRunning);
+    gameOver = true;
+  else if (!board.flat().some((e) => e == " ")) {
+    // only check if board is full when no victory condition was met
+    gameOver = true;
+    drawCondition = true;
+  }
+} while (!gameOver);
 
+// final render and victory message
 renderBoard();
-console.log(`${temp} won!!!`);
+console.log(!drawCondition ? `${activePlayerToken} won!!!` : "Draw");
